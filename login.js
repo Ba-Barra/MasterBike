060efc972ca87edeb0e0faff5f84ac6c7e42645a
Bayron <ba.barra@duocuc.cl>
2024-07-13 21:31:26 -0400

    paginas actualizadas

--- login.js
+++ login.js
@@ -12,11 +12,12 @@ loginForm.addEventListener('submit', function(event) {
     const usuario = usuarios.find(user => user.email === email && user.password === password);
 
     if (usuario) {
+        localStorage.setItem('sesionActiva', 'true');
 
         alert('Inicio de sesión exitoso');
 
+        window.location.href = 'solicitud_reparacion.html';
     } else {
-
         alert('Correo electrónico o contraseña incorrectos');
     }
 });
